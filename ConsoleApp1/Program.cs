de8f52bf59ee66f7a9159792f6e683c768c57020
Evgen Polivanov <polivanov004@gmail.com>
2025-03-21 13:59:59 +0300

    change intLit

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,13 +4,13 @@ class Program
 {
     static void Main()
     {
-        int intLiteral = 24;
+        int intLit = 24;
         double doubleLiteral = 3.14;
         char charLiteral = 'A';
         string stringLiteral = "Hello, World!";
         bool boolLiteral = true;
 
-        Console.WriteLine($"Integer Literal: {intLiteral}, Type: {intLiteral.GetType()}");
+        Console.WriteLine($"Integer Literal: {intLit}, Type: {intLit.GetType()}");
         Console.WriteLine($"Double Literal: {doubleLiteral}, Type: {doubleLiteral.GetType()}");
         Console.WriteLine($"Char Literal: {charLiteral}, Type: {charLiteral.GetType()}");
         Console.WriteLine($"String Literal: {stringLiteral}, Type: {stringLiteral.GetType()}");
